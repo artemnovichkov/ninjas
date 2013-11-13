bb1a11a01f0a3e4c93903ff5b55a8aef42e27553
NovichkovArtem <novichkoff93@gmail.com>
2013-11-13 12:41:13 +0700

    Base project

---
+++ NinjasTests/NinjasTests.m
@@ -0,0 +1,34 @@
+//
+//  NinjasTests.m
+//  NinjasTests
+//
+//  Created by Артем Новичков on 11/13/13.
+//  Copyright (c) 2013 Артем Новичков. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NinjasTests : XCTestCase
+
+@end
+
+@implementation NinjasTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
